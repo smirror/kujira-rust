4dc76d908d08a57a5ff5a0741a54344a83fa4853
libra <23025996+smirror@users.noreply.github.com>
2022-08-16 23:59:18 +0900

    add 3

---
+++ src/ch1/moon.py
@@ -0,0 +1,5 @@
+moon = 384400
+car = 80
+btrain = 300
+print("To moon for cat {} days".format(moon / car / 24))
+print("To moon for btrain {} days".format(moon / btrain / 24))
---
+++ src/ch1/moon.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let moon = 384400.0;
+    let car = 80.0;
+    let btrain = 120.0;
+    println!("To moon for cat {} days", moon / car / 24.0);
+    println!("To moon for btrain {} days", moon / btrain / 24.0);
+}
