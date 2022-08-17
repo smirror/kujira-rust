e7e7f53ebeeed7eec10c47c4a1c1f8aaa4d8aa8d
libra <23025996+smirror@users.noreply.github.com>
2022-08-17 21:41:17 +0900

    add caesar_enc

---
+++ src/ch1/caesar_enc.py
@@ -0,0 +1,19 @@
+def encrypt(text, shift):
+    code_a = ord('a')
+    code_z = ord('z')
+
+    result = ""
+    for ch in text:
+        code_ch = ord(ch)
+        if code_a <= code_ch <= code_z:
+            code_ch = (code_ch - code_a + shift) % 26 + code_a
+            if code_ch > code_z:
+                code_ch = code_ch - 26
+        result += chr(code_ch)
+
+    return result
+
+
+enc = encrypt("hello, world!", 3)
+dec = encrypt(enc, -3)
+print(enc, "=>", dec)
