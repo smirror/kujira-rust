03963567d6b27f8643abf9820eac6e1a741ba63a
libra <23025996+smirror@users.noreply.github.com>
2022-08-17 22:37:46 +0900

    remove target repo

--- src/ch2/hello/target/debug/deps/hello-ad7b689af84fd128.d
+++
@@ -1,5 +0,0 @@
-/home/libra/Project/kujira-rust/src/ch2/hello/target/debug/deps/hello-ad7b689af84fd128: src/main.rs
-
-/home/libra/Project/kujira-rust/src/ch2/hello/target/debug/deps/hello-ad7b689af84fd128.d: src/main.rs
-
-src/main.rs:
--- src/ch2/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/home/libra/Project/kujira-rust/src/ch2/hello/target/debug/hello: /home/libra/Project/kujira-rust/src/ch2/hello/src/main.rs
