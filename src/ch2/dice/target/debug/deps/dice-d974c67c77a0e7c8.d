92c64a408639356e305debf00b74f45db9851dee
libra <23025996+smirror@users.noreply.github.com>
2022-08-20 00:02:19 +0900

    add dice

--- src/ch2/dice/target/debug/deps/dice-d974c67c77a0e7c8.d
+++
@@ -1,5 +0,0 @@
-/home/libra/Project/kujira-rust/src/ch2/dice/target/debug/deps/dice-d974c67c77a0e7c8: src/main.rs
-
-/home/libra/Project/kujira-rust/src/ch2/dice/target/debug/deps/dice-d974c67c77a0e7c8.d: src/main.rs
-
-src/main.rs:
--- src/ch2/dice/target/debug/dice.d
+++
@@ -1,1 +0,0 @@
-/home/libra/Project/kujira-rust/src/ch2/dice/target/debug/dice: /home/libra/Project/kujira-rust/src/ch2/dice/src/main.rs
