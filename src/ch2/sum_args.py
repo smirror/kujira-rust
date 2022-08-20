facf9ffdb659416ed55d745f51a4f4e344ac26b6
libra <23025996+smirror@users.noreply.github.com>
2022-08-20 15:42:24 +0900

    add sum_args

---
+++ src/ch2/sum_args.py
@@ -0,0 +1,14 @@
+import sys
+
+total = 0
+
+for i, v in enumerate(sys.argv):
+    if i == 0:
+        continue
+
+    try:
+        total += float(v)
+    except ValueError:
+        pass
+
+print(total)
