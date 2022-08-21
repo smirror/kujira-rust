370b6f56a41509f8fe501b334d9576739424254d
smirror <23025996+smirror@users.noreply.github.com>
2022-08-21 12:49:08 +0000

    Automated Cargo fmt changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 trait TreasureBox {
-    fn open(&self, key_no: i32) -> bool { self.get_key_no() == key_no }
+    fn open(&self, key_no: i32) -> bool {
+        self.get_key_no() == key_no
+    }
     fn check(&self);
     fn get_key_no(&self) -> i32;
 }
@@ -26,7 +28,9 @@ impl TreasureBox for EmptyBox {
     fn check(&self) {
         println!("空箱だった。");
     }
-    fn get_key_no(&self) -> i32 { self.key_no }
+    fn get_key_no(&self) -> i32 {
+        self.key_no
+    }
 }
 
 fn open_box(tbox: &impl TreasureBox, key_no: i32) {
@@ -38,9 +42,15 @@ fn open_box(tbox: &impl TreasureBox, key_no: i32) {
 }
 
 fn main() {
-    let box1 = JewelryBox { price: 30, key_no: 1 };
+    let box1 = JewelryBox {
+        price: 30,
+        key_no: 1,
+    };
     let box2 = EmptyBox { key_no: 1 };
-    let box3 = JewelryBox { price: 20, key_no: 2 };
+    let box3 = JewelryBox {
+        price: 20,
+        key_no: 2,
+    };
 
     let my_key = 2;
     open_box(&box1, my_key);
