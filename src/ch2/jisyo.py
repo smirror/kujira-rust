48dc4e908258081bf125af444635af6cda166519
smirror <23025996+smirror@users.noreply.github.com>
2022-08-21 04:36:03 +0000

    Automated Python formatter changes

--- src/ch2/jisyo.py
+++ src/ch2/jisyo.py
@@ -11,6 +11,7 @@ word = sys.argv[1]
 with open(dicfile, "rt", encoding="utf-8") as fp:
     while True:
         line = fp.readline()
-        if not line: break
+        if not line:
+            break
         if word in line:
             print(line.strip())
