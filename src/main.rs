79ddc2386cb00e80ccba145090704c4cfd5e0a58
smirror <23025996+smirror@users.noreply.github.com>
2022-08-21 03:53:12 +0000

    Automated Cargo fmt changes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     let mut total: f64 = 0.0;
 
     for (i, fname) in args.enumerate() {
-        if i == 0 { continue; }
+        if i == 0 {
+            continue;
+        }
         let text = fs::read_to_string(fname).unwrap();
         let lines = text.split('\n');
         for line in lines {
